aabf17401cb66608f989e67b1cdb828f690bd451
Heinz Riener <heinz.riener@gmail.com>
2020-05-21 11:12:57 +0200

    Draft of `fanout_limit_view`.

--- include/mockturtle/utils/node_map.hpp
+++ include/mockturtle/utils/node_map.hpp
@@ -226,6 +226,32 @@ public:
   {
   }
 
+  auto begin()
+  {
+    return std::begin( *data );
+  }
+
+  auto begin() const
+  {
+    return std::begin( *data );
+  }
+
+  auto end()
+  {
+    return std::end( *data );
+  }
+
+  auto end() const
+  {
+    return std::end( *data );
+  }
+
+  /*! \brief Returns an iterator to data */
+  auto find( node const& n ) const
+  {
+    return data->find( ntk.node_to_index( n ) );
+  }
+
   /*! \brief Check if a key is already defined. */
   bool has( node const& n ) const
   {
