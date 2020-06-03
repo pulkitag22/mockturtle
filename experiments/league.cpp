7fa8d71e8a9b5d368ee3c03607da627bf39a6f6c
Heinz Riener <heinz.riener@gmail.com>
2020-06-03 15:08:28 +0200

    league.

--- experiments/league.cpp
+++ experiments/league.cpp
@@ -4,6 +4,7 @@
 #include <mockturtle/algorithms/cut_rewriting.hpp>
 #include <mockturtle/algorithms/mig_algebraic_rewriting.hpp>
 #include <mockturtle/algorithms/node_resynthesis/mig_npn.hpp>
+#include <mockturtle/algorithms/mig_resub.hpp>
 #include <mockturtle/io/aiger_reader.hpp>
 #include <mockturtle/views/depth_view.hpp>
 #include <lorina/lorina.hpp>
@@ -11,6 +12,8 @@
 #include <fmt/format.h>
 #include <vector>
 
+/*! \brief A benchmark
+ */
 template<class Ntk>
 class benchmark
 {
@@ -26,11 +29,16 @@ public:
     return name_;
   }
 
-  Ntk network() const
+  Ntk clone() const
+  {
+    return cleanup_dangling( ntk_ );
+  }
+
+  Ntk const& get() const
   {
     return ntk_;
   }
-  
+
 protected:
   Ntk ntk_;
   std::string name_;
@@ -39,222 +47,127 @@ protected:
 struct agent_statistics
 {
   mockturtle::stopwatch<>::duration time_total{0};
-  int64_t score{0};
-};
+  uint64_t size_before{0u};
+  uint64_t size_after{0u};
 
-template<class Ntk>
-class agent
-{
-public:
-  virtual ~agent() {}
+  uint64_t depth_before{0u};
+  uint64_t depth_after{0u};
 
-  virtual std::string name() const = 0;
-  virtual void run( Ntk ntk ) = 0;
-  virtual void report() const = 0;
-};
-
-class dfs_depth_rewriting : public agent<mockturtle::mig_network>
-{
-public:
-  virtual std::string name() const
+  void report() const
   {
-    return std::string{"mig::depth_rewriting[dfs]"};
+    fmt::print( "[i] time: {:>8.2f}\n[i] size:  {:6d} -> {:6d} ({:d})\n[i] depth: {:6d} -> {:6d} ({:d})\n",
+                mockturtle::to_seconds( time_total ),
+                size_before, size_after, int64_t( size_before ) - int64_t( size_after ),
+                depth_before, depth_after, int64_t(depth_after) - depth_before );
   }
-  
-  virtual void run( mockturtle::mig_network ntk )
-  {
-    mockturtle::depth_view<mockturtle::mig_network> depth_ntk( ntk );
-    
-    mockturtle::stopwatch t( st.time_total );
-    uint64_t const before = depth_ntk.size();
+};
 
+/*! \brief Algebraic depth rewriting for MIGs
+ */
+class mig_alg_rw
+{
+public:
+  template<typename Ntk>
+  void run( Ntk& ntk )
+  {
     mockturtle::mig_algebraic_depth_rewriting_params ps;
     ps.strategy = mockturtle::mig_algebraic_depth_rewriting_params::dfs;
     ps.allow_area_increase = false;
-    mockturtle::mig_algebraic_depth_rewriting( depth_ntk, ps );
-    
-    st.score += ( before - depth_ntk.size() );
+    mockturtle::mig_algebraic_depth_rewriting( ntk, ps );
   }
-
-  virtual void report() const
-  {
-    fmt::print( "[i] {} score: {:8d} time: {:>8.2f}\n",
-                name(), st.score, mockturtle::to_seconds( st.time_total ) );
-  }
-  
-protected:
-  agent_statistics st;
 };
 
-class aggressive_depth_rewriting : public agent<mockturtle::mig_network>
+/*! \brief Boolean resubstitution for MIGs
+ */
+class mig_rs
 {
 public:
-  virtual std::string name() const
+  template<typename Ntk>
+  void run( Ntk& ntk )
   {
-    return std::string{"mig::depth_rewriting[dfs]"};
-  }
-  
-  virtual void run( mockturtle::mig_network ntk )
-  {
-    mockturtle::depth_view<mockturtle::mig_network> depth_ntk( ntk );
-    
-    mockturtle::stopwatch t( st.time_total );
-    uint64_t const before = depth_ntk.size();
+    mockturtle::resubstitution_params ps;
+    ps.max_pis = 8u;
+    ps.max_inserts = 1u;
+    ps.progress = true;
 
-    mockturtle::mig_algebraic_depth_rewriting_params ps;
-    ps.strategy = mockturtle::mig_algebraic_depth_rewriting_params::aggressive;
-    ps.allow_area_increase = false;
-    mockturtle::mig_algebraic_depth_rewriting( depth_ntk, ps );
-    
-    st.score += ( before - depth_ntk.size() );
-  }
-
-  virtual void report() const
-  {
-    fmt::print( "[i] {} score: {:8d} time: {:>8.2f}\n",
-                name(), st.score, mockturtle::to_seconds( st.time_total ) );
-  }
-  
-protected:
-  agent_statistics st;
-};
-
-class selective_depth_rewriting : public agent<mockturtle::mig_network>
-{
-public:
-  virtual std::string name() const
-  {
-    return std::string{"mig::depth_rewriting[dfs]"};
-  }
-  
-  virtual void run( mockturtle::mig_network ntk )
-  {
-    mockturtle::depth_view<mockturtle::mig_network> depth_ntk( ntk );
-    
-    mockturtle::stopwatch t( st.time_total );
-    uint64_t const before = depth_ntk.size();
-
-    mockturtle::mig_algebraic_depth_rewriting_params ps;
-    ps.strategy = mockturtle::mig_algebraic_depth_rewriting_params::selective;
-    ps.allow_area_increase = false;
-    mockturtle::mig_algebraic_depth_rewriting( depth_ntk, ps );
-    
-    st.score += ( before - depth_ntk.size() );
-  }
-
-  virtual void report() const
-  {
-    fmt::print( "[i] {} score: {:8d} time: {:>8.2f}\n",
-                name(), st.score, mockturtle::to_seconds( st.time_total ) );
-  }
-  
-protected:
-  agent_statistics st;
-};
-
-class migarw : public agent<mockturtle::mig_network>
-{
-public:
-  virtual std::string name() const
-  {
-    return std::string{"migarw"};
-  }
-  
-  virtual void run( mockturtle::mig_network ntk )
-  {
-    mockturtle::depth_view<mockturtle::mig_network> depth_ntk( ntk );
-    
-    mockturtle::stopwatch t( st.time_total );
-    uint64_t const before = depth_ntk.size();
-
-    // mockturtle::mig_algebraic_depth_rewriting_params arw_ps;
-    // arw_ps.strategy = mockturtle::mig_algebraic_depth_rewriting_params::aggressive;
-
-    // mockturtle::mig_algebraic_depth_rewriting_stats arw_st;
-    mockturtle::mig_algebraic_depth_rewriting( depth_ntk );
-    
-    st.score += ( before - depth_ntk.size() );
-  }
-
-  virtual void report() const
-  {
-    fmt::print( "[i] {} score: {:8d} time: {:>8.2f}\n",
-                name(), st.score, mockturtle::to_seconds( st.time_total ) );
-  }
-  
-protected:
-  agent_statistics st;
-};
-
-class migrw : public agent<mockturtle::mig_network>
-{
-public:
-  virtual std::string name() const
-  {
-    return std::string{"migrw"};
-  }
-  
-  virtual void run( mockturtle::mig_network ntk )
-  {
-    mockturtle::stopwatch t( st.time_total );
-
-    uint64_t const before = ntk.num_gates();
-    mockturtle::mig_npn_resynthesis resyn;
-    mockturtle::cut_rewriting_params rw_ps;
-    rw_ps.cut_enumeration_ps.cut_size = 4;
-    cut_rewriting_with_compatibility_graph( ntk, resyn, rw_ps );
+    mockturtle::mig_resubstitution( ntk, ps );
     ntk = cleanup_dangling( ntk );
-    st.score = before - ntk.num_gates();
   }
-
-  virtual void report() const
-  {
-    fmt::print( "[i] {} score: {:8d} time: {:>8.2f}\n",
-                name(), st.score, mockturtle::to_seconds( st.time_total ) );
-  }
-  
-protected:
-  agent_statistics st;
 };
 
-class migrw2 : public agent<mockturtle::mig_network>
+/*! \brief Boolean rewriting for MIGs
+ */
+class mig_rw
 {
 public:
-  virtual std::string name() const
+  template<typename Ntk>
+  void run( Ntk& ntk )
   {
-    return std::string{"migrw2"};
-  }
-  
-  virtual void run( mockturtle::mig_network ntk )
-  {
-    mockturtle::stopwatch t( st.time_total );
-
-    uint64_t const before = ntk.num_gates();
     mockturtle::mig_npn_resynthesis resyn;
-    mockturtle::cut_rewriting_params rw_ps;
-    rw_ps.cut_enumeration_ps.cut_size = 4;
-    cut_rewriting( ntk, resyn, rw_ps );
+    mockturtle::cut_rewriting_params ps;
+    ps.cut_enumeration_ps.cut_size = 4;
+    cut_rewriting_with_compatibility_graph( ntk, resyn, ps );
     ntk = cleanup_dangling( ntk );
-    st.score = before - ntk.num_gates();
   }
-
-  virtual void report() const
-  {
-    fmt::print( "[i] {} score: {:8d} time: {:>8.2f}\n",
-                name(), st.score, mockturtle::to_seconds( st.time_total ) );
-  }
-  
-protected:
-  agent_statistics st;
 };
 
+namespace detail
+{
+
+template<typename Ntk, typename T>
+void run_agents( Ntk& ntk, T&& agent )
+{
+  agent.run( ntk );
+}
+
+template<typename Ntk, typename T, typename ... Ts>
+void run_agents( Ntk& ntk, T&& agent, Ts ... ts )
+{
+  agent.run( ntk );
+  run_agents<Ntk, Ts...>( ntk, std::forward<Ts>(ts)... );
+}
+
+} /* detail */
+
+template<typename... AgentTypes>
+class flow
+{
+public:
+  template<typename... T>
+  explicit flow()
+  {
+    static_assert( ( sizeof...( AgentTypes ) > 0 ), "at least one agent must be specified" );
+    st.resize( sizeof...( AgentTypes ) );
+  }
+
+  template<typename Benchmark>
+  void run( Benchmark const& bm )
+  {
+    auto ntk = bm.clone();
+
+    std::cout << "size_before = " << ntk.size() << std::endl;
+
+    std::apply( [&]( auto&&... args ){
+        detail::run_agents( ntk, std::forward<decltype( args )>( args )... );
+      },
+      agents );
+
+    std::cout << "size_after = " << ntk.size() << std::endl;
+  }
+
+private:
+  std::tuple<AgentTypes...> agents;
+  std::vector<agent_statistics> st;
+};
+
+#if 0
 class league
 {
 public:
   using network_type = mockturtle::mig_network;
   using benchmark_type = std::shared_ptr<benchmark<network_type>>;
   using agent_type = std::shared_ptr<agent<network_type>>;
-  
+
 public:
   explicit league()
   {
@@ -263,9 +176,9 @@ public:
   void run()
   {
     /* load benchmarks */
-    fmt::print( "[i] agents = {}\n", agents.size() );    
+    fmt::print( "[i] agents = {}\n", agents.size() );
     fmt::print( "[i] benchmarks = {}\n", benchmarks.size() );
-    
+
     for  ( auto const& benchmark : benchmarks )
     {
       for ( auto const& agent : agents )
@@ -286,7 +199,7 @@ public:
   {
     benchmarks.emplace_back( std::make_shared<benchmark<network_type>>( ntk, name ) );
   }
-  
+
   template<class T>
   void add_agent()
   {
@@ -295,36 +208,34 @@ public:
 
 protected:
   std::vector<benchmark_type> benchmarks;
-  std::vector<agent_type> agents; 
+  std::vector<agent_type> agents;
 };
+#endif
 
 int main()
 {
-  league l;
+  /* define some optimization flows */
+  flow<mig_rw, mig_rs, mig_rw, mig_rs, mig_rw, mig_rs> f0;
+  flow<mig_rw, mig_rw, mig_rw, mig_rs, mig_rs, mig_rs> f1;
 
-  /* add agents */
-  l.add_agent<dfs_depth_rewriting>();
-  l.add_agent<aggressive_depth_rewriting>();
-  l.add_agent<selective_depth_rewriting>();
-  // l.add_agent<migrw>();
-  // l.add_agent<migrw2>();
-  
   /* add benchmarks */
-  for ( auto const& benchmark : experiments::epfl_benchmarks( ~experiments::hyp ) )
+  for ( auto const& benchmark : experiments::epfl_benchmarks( experiments::adder | experiments::bar | experiments::cavlc | experiments::dec | experiments::priority ) )
   {
+    fmt::print( "[i] processig {}\n", benchmark );
+
     mockturtle::mig_network mig;
     if ( lorina::read_aiger( experiments::benchmark_path( benchmark ), mockturtle::aiger_reader( mig ) ) == lorina::return_code::success )
     {
-      l.add_benchmark( mig, benchmark );
+      ::benchmark<mockturtle::mig_network> b( mig, benchmark );
+
+      f0.run( b );
+      f1.run( b );
     }
     else
     {
       fmt::print( "[i] parsing benchmark failed\n" );
     }
   }
-  
-  /* evaluate agents on benchmarks */
-  l.run();
-  
+
   return 0;
 }
