94d39694aab7e90f6e2de42bb27673ac9e590f46
Heinz Riener <heinz.riener@gmail.com>
2020-06-04 09:00:31 +0200

    flow experiments with flow modifiers.

--- experiments/league.cpp
+++ experiments/league.cpp
@@ -70,10 +70,14 @@ public:
   template<typename Ntk>
   void run( Ntk& ntk )
   {
+    mockturtle::depth_view depth_ntk{ntk};
+
     mockturtle::mig_algebraic_depth_rewriting_params ps;
     ps.strategy = mockturtle::mig_algebraic_depth_rewriting_params::dfs;
     ps.allow_area_increase = false;
-    mockturtle::mig_algebraic_depth_rewriting( ntk, ps );
+    mockturtle::mig_algebraic_depth_rewriting( depth_ntk, ps );
+
+    ntk = cleanup_dangling( ntk );
   }
 };
 
@@ -111,20 +115,157 @@ public:
   }
 };
 
+template<typename Agent, uint32_t times>
+class repeat
+{
+public:
+  template<typename Ntk>
+  void run( Ntk& ntk )
+  {
+    Agent a;
+    for ( auto i = 0u; i < times; ++i )
+    {
+      a.run( ntk );
+    }
+  }
+};
+
+template<typename Agent0, typename Agent1>
+class combine
+{
+public:
+  template<typename Ntk>
+  void run( Ntk& ntk )
+  {
+    Agent0().run( ntk );
+    Agent1().run( ntk );
+  }
+};
+
+class size_eval
+{
+public:
+  template<typename Ntk>
+  void run( Ntk& ntk )
+  {
+    std::cout << ntk.size() << std::endl;
+  }
+};
+
+class network_size_evaluator
+{
+public:
+  explicit network_size_evaluator()
+  {
+  }
+
+  template<typename Ntk>
+  void record( Ntk const& ntk )
+  {
+    sizes.emplace_back( ntk.size() );
+  }
+
+  void reset()
+  {
+    sizes.clear();
+  }
+
+  void report( std::string const& name = {} ) const
+  {
+    fmt::print( "[i] {}: ", name );
+    for ( const auto& s : sizes )
+    {
+      fmt::print( "({})", s );
+    }
+    std::cout << std::endl;
+  }
+
+protected:
+  std::vector<uint64_t> sizes;
+};
+
+class network_depth_evaluator
+{
+public:
+  explicit network_depth_evaluator()
+  {
+  }
+
+  template<typename Ntk>
+  void record( Ntk const& ntk )
+  {
+    mockturtle::depth_view depth_ntk{ntk};
+    depths.emplace_back( depth_ntk.depth() );
+  }
+
+  void reset()
+  {
+    depths.clear();
+  }
+
+  void report( std::string const& name = {} ) const
+  {
+    fmt::print( "[i] {}: ", name );
+    for ( const auto& d : depths )
+    {
+      fmt::print( "({})", d );
+    }
+    std::cout << std::endl;
+  }
+
+protected:
+  std::vector<uint64_t> depths;
+};
+
+class network_size_and_depth_evaluator
+{
+public:
+  explicit network_size_and_depth_evaluator()
+  {
+  }
+
+  template<typename Ntk>
+  void record( Ntk const& ntk )
+  {
+    mockturtle::depth_view depth_ntk{ntk};
+    data.emplace_back( std::make_tuple( depth_ntk.size(), depth_ntk.depth() ) );
+  }
+
+  void reset()
+  {
+    data.clear();
+  }
+
+  void report( std::string const& name = {} ) const
+  {
+    fmt::print( "[i] {}: ", name );
+    for ( const auto& tup : data )
+    {
+      fmt::print( "({}, {})", std::get<0>( tup ), std::get<1>( tup ) );
+    }
+    std::cout << std::endl;
+  }
+
+protected:
+  std::vector<std::tuple<uint64_t,uint64_t>> data;
+};
+
 namespace detail
 {
 
-template<typename Ntk, typename T>
-void run_agents( Ntk& ntk, T&& agent )
+template<typename Ntk, typename S, typename T>
+void run_agents_on_network( Ntk& ntk, S& st, T&& agent )
 {
   agent.run( ntk );
+  st.record( ntk );
 }
 
-template<typename Ntk, typename T, typename ... Ts>
-void run_agents( Ntk& ntk, T&& agent, Ts ... ts )
+template<typename Ntk, typename S, typename T, typename ... Ts>
+void run_agents_on_network( Ntk& ntk, S& st, T&& agent, Ts ... ts )
 {
   agent.run( ntk );
-  run_agents<Ntk, Ts...>( ntk, std::forward<Ts>(ts)... );
+  st.record( ntk );
+  run_agents_on_network<Ntk, S, Ts...>( ntk, st, std::forward<Ts>( ts )... );
 }
 
 } /* detail */
@@ -137,27 +278,24 @@ public:
   explicit flow()
   {
     static_assert( ( sizeof...( AgentTypes ) > 0 ), "at least one agent must be specified" );
-    st.resize( sizeof...( AgentTypes ) );
   }
 
-  template<typename Benchmark>
-  void run( Benchmark const& bm )
+  template<typename Benchmark, typename Evaluator>
+  auto run( Benchmark const& bm, Evaluator& eval )
   {
     auto ntk = bm.clone();
-
-    std::cout << "size_before = " << ntk.size() << std::endl;
+    eval.record( ntk );
 
     std::apply( [&]( auto&&... args ){
-        detail::run_agents( ntk, std::forward<decltype( args )>( args )... );
+        detail::run_agents_on_network( ntk, eval, std::forward<decltype( args )>( args )... );
       },
       agents );
 
-    std::cout << "size_after = " << ntk.size() << std::endl;
+    return ntk;
   }
 
 private:
   std::tuple<AgentTypes...> agents;
-  std::vector<agent_statistics> st;
 };
 
 #if 0
@@ -212,30 +350,117 @@ protected:
 };
 #endif
 
+/* TODOs:
+ * - time stamps
+ * - plots
+ * - cec
+ * - flow modifiers
+ */
 int main()
 {
   /* define some optimization flows */
-  flow<mig_rw, mig_rs, mig_rw, mig_rs, mig_rw, mig_rs> f0;
-  flow<mig_rw, mig_rw, mig_rw, mig_rs, mig_rs, mig_rs> f1;
+  flow<mig_rw, mig_rw, mig_rw> f0;
+  flow<mig_rs, mig_rs, mig_rs> f1;
+  flow<mig_rw, mig_rs, mig_rw, mig_rs, mig_rw, mig_rs> f2;
+  flow<mig_rw, mig_rw, mig_rw, mig_rs, mig_rs, mig_rs> f3;
+  flow<mig_alg_rw, mig_rw, mig_rw, mig_rw, mig_rs, mig_rs, mig_rs> f4;
+
+  /* flow with flow modifiers and evaluators */
+  flow<size_eval,repeat<combine<combine<mig_rw,mig_rs>,size_eval>,3u>> f5;
 
   /* add benchmarks */
   for ( auto const& benchmark : experiments::epfl_benchmarks( experiments::adder | experiments::bar | experiments::cavlc | experiments::dec | experiments::priority ) )
   {
-    fmt::print( "[i] processig {}\n", benchmark );
+    fmt::print( "[i] processing {}\n", benchmark );
 
     mockturtle::mig_network mig;
+    network_size_and_depth_evaluator eval;
+
     if ( lorina::read_aiger( experiments::benchmark_path( benchmark ), mockturtle::aiger_reader( mig ) ) == lorina::return_code::success )
     {
       ::benchmark<mockturtle::mig_network> b( mig, benchmark );
 
-      f0.run( b );
-      f1.run( b );
+      {
+        eval.reset();
+        auto const new_ntk = f0.run( b, eval );
+        eval.report( "(RW)^3" );
+        auto const cec = experiments::abc_cec( new_ntk, benchmark );
+        std::cout << "[i] cec = " << cec << std::endl;
+      }
+
+      {
+        eval.reset();
+        auto const new_ntk = f1.run( b, eval );
+        eval.report( "(RS)^3" );
+        auto const cec = experiments::abc_cec( new_ntk, benchmark );
+        std::cout << "[i] cec = " << cec << std::endl;
+      }
+
+      {
+        eval.reset();
+        auto const new_ntk = f2.run( b, eval );
+        eval.report( "(RW;RS)^3" );
+        auto const cec = experiments::abc_cec( new_ntk, benchmark );
+        std::cout << "[i] cec = " << cec << std::endl;
+      }
+
+      {
+        eval.reset();
+        auto const new_ntk = f3.run( b, eval );
+        eval.report( "RW^3;RS^3" );
+        auto const cec = experiments::abc_cec( new_ntk, benchmark );
+        std::cout << "[i] cec = " << cec << std::endl;
+      }
+
+      {
+        eval.reset();
+        auto const new_ntk = f4.run( b, eval );
+        eval.report( "ARW; (RW;RS)^3" );
+        auto const cec = experiments::abc_cec( new_ntk, benchmark );
+        std::cout << "[i] cec = " << cec << std::endl;
+      }
+
+      {
+        eval.reset();
+        auto const new_ntk = f5.run( b, eval );
+        eval.report( "REPEAT[COMBINE[RW,RS],3]" );
+        auto const cec = experiments::abc_cec( new_ntk, benchmark );
+        std::cout << "[i] cec = " << cec << std::endl;
+      }
     }
     else
     {
       fmt::print( "[i] parsing benchmark failed\n" );
     }
   }
+
+#if 0
+  /* does not compile */
+  flow<mig_alg_rw, mig_rw, mig_rw, mig_rw, mig_rs, mig_rs, mig_rs> f2;
+
+  /* add benchmarks */
+  for ( auto const& benchmark : experiments::epfl_benchmarks( experiments::adder | experiments::bar | experiments::cavlc | experiments::dec | experiments::priority ) )
+  {
+    fmt::print( "[i] processing {}\n", benchmark );
+
+    mockturtle::mig_network mig;
+    mockturtle::depth_view<mockturtle::mig_network> depth_mig{mig};
+    network_size_and_depth_evaluator eval;
+
+    if ( lorina::read_aiger( experiments::benchmark_path( benchmark ), mockturtle::aiger_reader( mig ) ) == lorina::return_code::success )
+    {
+      ::benchmark<mockturtle::depth_view<mockturtle::mig_network>> b( depth_mig, benchmark );
+
+      eval.reset();
+      f2.run( b, eval );
+      eval.report( "NEW FLOW" );
+    }
+    else
+    {
+      fmt::print( "[i] parsing benchmark failed\n" );
+    }
+  }
+#endif
 
   return 0;
 }
